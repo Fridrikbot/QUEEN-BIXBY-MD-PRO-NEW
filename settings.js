f7f9f5e0fce99267931dcc0667e9993b20d1e6a1
Fridrikbot <110393599+Fridrikbot@users.noreply.github.com>
2022-08-04 19:03:35 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
+global.owner = ['+212672047604']
+global.premium = ['+212672047604']
+global.ownernomer = '+212672047604'
 global.ownername = 'Dark Alpha'
 global.botname = 'BIXBY-MD USER BOT'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
